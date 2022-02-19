3ce6b117b4754b8620693e8bcc7744e13c20c824
Mark Lester Cuevo <lester.cuevo.mlc@gmail.com>
2022-02-19 14:40:38 +0800

    Added multiplication and division feature. Created a new file

--- calculator.py
+++ calculator.py
@@ -3,5 +3,9 @@ n2 = int(input('Enter second number : '))
 
 sum = n1 + n2
 dif = n1 - n2
+multi = n1 * n2
+qoutient = n1 / n2
 print('Sum =', sum)
 print('minus =', dif)
+print('multi =', multi)
+print('Div =', qoutient)
